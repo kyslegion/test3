52f464b2a34731247756c29f03b71d311d195b42
kyslegion <kyslegion@gmail.com>
2022-09-29 16:02:06 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import express from 'express';
 let app=express();
+let path =require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname+ '/'));
 
 app.get('/',(req,res)=>{
-    res.json({
-        hello:"hi",
-    })
-})
+    console.log(req.route.path);
+    res.sendFile(path.join(__dirname, './connexion.html'));
+});
+// app.get('/',(req,res)=>{
+//     res.json({
+//         hello:"hi",
+//     })
+// })
+
 
 
 
